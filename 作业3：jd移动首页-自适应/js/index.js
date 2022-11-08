cdba5c8bd861a7ff0a2ca5e88e3f82377d7ab3f6
xiaokai <1020849724@qq.com>
2022-11-08 20:29:19 +0800

    作业3：jd移动首页-自适应完成

--- 作业3：jd移动首页-自适应/js/index.js
+++ 作业3：jd移动首页-自适应/js/index.js
@@ -2,9 +2,15 @@
 let html = document.documentElement;
 console.log(html);
 function setRem() {
-  let ui_w = 375;
+  let ui_w = 640;
   let cl_w = document.documentElement.clientWidth || document.body.clientWidth;
+  cl_w = cl_w < 300 ? 300 : cl_w;
+  cl_w = cl_w > 800 ? 800 : cl_w;
   html.style.fontSize = (cl_w / ui_w) * 10 + "px";
 }
-window.onresize = setRem;
+let timer = null;
+window.onresize = function () {
+  clearTimeout(timer);
+  timer = setTimeout(setRem, 50);
+};
 window.onload = setRem;
